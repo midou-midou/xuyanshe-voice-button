4ddbab7e7678f9bd234eaaf97d606decd03c17df
midou <15500615701@163.com>
2021-11-04 23:03:01 +0800

    feat: 小希吃鸡语音包、分离voice、翻译支持

--- src/component/utills/SupportBtn/index.jsx
+++ src/component/utills/SupportBtn/index.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 function SupportBtn() {
     const supportInfo = useSelector(state => state.getSiteInfo.supports);
+    const tsupportInfo = useSelector(state => state.getSiteInfo.translateSupport);
     const vup = useSelector(state => state.getVupData);
     const lang = useSelector((state) => state.getLang);
     const [linkBase] = useState('//space.bilibili.com/');
@@ -22,6 +23,22 @@ function SupportBtn() {
                         let color = findVup(vup, item.tag).color;
                         return (
                             <div className="btn-wrapper btn-support" style={{backgroundColor: `${color}`,'--support-color':`${color}`}} key={key}>
+                                <a className="btn-name support-name" href={`${linkBase}${item.uid}`}>{item.name}</a>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="panel-info-container">
+                <IntlProvider locale={lang} messages={{zh:'翻译支持',en:'translate Support',jp:'翻訳サポート'}}>
+                    <div className="panel-tags"><FormattedMessage id={lang}></FormattedMessage></div>
+                </IntlProvider>
+            </div>
+            <div className="panel-btn-container">
+                {
+                    tsupportInfo.map((item, key) => {
+                        return (
+                            <div className="btn-wrapper btn-support" style={{backgroundColor: `var(--link-blue)`,'--support-color':`var(--link-blue)`}} key={key}>
                                 <a className="btn-name support-name" href={`${linkBase}${item.uid}`}>{item.name}</a>
                             </div>
                         )
---
+++ src/config/voices/xiaoxi.js
@@ -0,0 +1,567 @@
+exports.xiaoxi = [
+    {
+        clfy: {zh: 'AI PUBG', en: '', jp: ''},
+        alias: {zh: '能赢吗', en: '', jp: ''},
+        voice:[
+            {
+                path: 'xx-nihaoa.mp3',
+                desc: {
+                    zh: '你好啊兄die~',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-wsxx.mp3',
+                desc: {
+                    zh: '你好我是AI小希',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-aijiushijiqir.mp3',
+                desc: {
+                    zh: 'AI就是机器人',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-bzhanlaide.mp3',
+                desc: {
+                    zh: 'B站来的',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-bzhanjiushitongxjy.mp3',
+                desc: {
+                    zh: 'B站就是同性交友网站?',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-zenmch.mp3',
+                desc: {
+                    zh: '怎么称呼你呢',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-wysxx.mp3',
+                desc: {
+                    zh: '巧了吗这不是,我也是小希',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-bsqnywzmkam.mp3',
+                desc: {
+                    zh: '变声器能有我这么可爱吗',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-qflh.mp3',
+                desc: {
+                    zh: '枪法能这么厉害',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-youdirenxx.mp3',
+                desc: {
+                    zh: '有敌人小心呀',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-nabianyoudir.mp3',
+                desc: {
+                    zh: '那边有敌人',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-wgnyddx.mp3',
+                desc: {
+                    zh: '我给你点物资吧',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-xxxx.mp3',
+                desc: {
+                    zh: '小心小心',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-data1.mp3',
+                desc: {
+                    zh: '打他打他~',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-data2.mp3',
+                desc: {
+                    zh: '打他打他⬆',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-kxkx.mp3',
+                desc: {
+                    zh: '哎可惜',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-a.mp3',
+                desc: {
+                    zh: '啊~?',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-mashang.mp3',
+                desc: {
+                    zh: '马上',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-hundan.mp3',
+                desc: {
+                    zh: '魂淡',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-huaq.mp3',
+                desc: {
+                    zh: '花Q（KizunaAI bushi',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-hao.mp3',
+                desc: {
+                    zh: '好!',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-dqkgll.mp3',
+                desc: {
+                    zh: '毒圈快过来了',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-paxpax.mp3',
+                desc: {
+                    zh: '趴下~趴下~',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-wtdjwshm.mp3',
+                desc: {
+                    zh: '听得见我说话吗',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-wei.mp3',
+                desc: {
+                    zh: '喂?',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-ylc.mp3',
+                desc: {
+                    zh: '有辆车',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-wlk.mp3',
+                desc: {
+                    zh: '我来开',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-zbzb.mp3',
+                desc: {
+                    zh: '这边这边',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-wxyq.mp3',
+                desc: {
+                    zh: '我需要枪',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-wxyy.mp3',
+                desc: {
+                    zh: '我需要药',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-wzql.mp3',
+                desc: {
+                    zh: '我中枪了',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-wbddl.mp3',
+                desc: {
+                    zh: '我被打到了',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-wbxlknml.mp3',
+                desc: {
+                    zh: '我不行了,靠你们了',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-jywmszbd.mp3',
+                desc: {
+                    zh: '加油我们是最棒的',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-sxdjgwl.mp3',
+                desc: {
+                    zh: '你们安心的去吧,剩下的交给我啦',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-binihaida.mp3',
+                desc: {
+                    zh: '小希比你还大呢',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-binihaida1.mp3',
+                desc: {
+                    zh: '小希比你还大呢1',
+                    en: '',
+                    jp: ''
+                }
+            }
+        ]
+    },
+    {
+        clfy: {zh: '小希动物园', en: '', jp: ''},
+        alias: {zh: 'We Bought A Zoo', en: '', jp: ''},
+        voice:[
+            {
+                path: 'xx-dog2.mp3',
+                desc: {
+                    zh: '哈士奇叫',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-dog111.mp3',
+                desc: {
+                    zh: '哈士奇又叫',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-ji1.mp3',
+                desc: {
+                    zh: '鸡叫',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-luotuo1.mp3',
+                desc: {
+                    zh: '骆驼叫',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-niao1.mp3',
+                desc: {
+                    zh: '鸟叫',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-wolf1.mp3',
+                desc: {
+                    zh: '狼叫?',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-yangtuo.mp3',
+                desc: {
+                    zh: '羊驼叫',
+                    en: '',
+                    jp: ''
+                }
+            }
+        ]
+    },
+    {
+        clfy: {zh: '变态', en: '', jp: ''},
+        alias: {zh: '变态吗?',en: '', jp: ''},
+        voice:[
+            {
+                path: 'xx-aa.mp3',
+                desc: {
+                    zh: '啊啊啊啊啊啊啊啊',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-biantai.mp3',
+                desc: {
+                    zh: '变态',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-biexiapianle.mp3',
+                desc: {
+                    zh: '下片',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-ge.mp3',
+                desc: {
+                    zh: '嗝~',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-gunxn.mp3',
+                desc: {
+                    zh: '滚*n',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-laogongx7.mp3',
+                desc: {
+                    zh: '老公老公老公老公老公老公老公',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-morenjia.mp3',
+                desc: {
+                    zh: '别摸我＞﹏＜',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'xx-zoukai.mp3',
+                desc: {
+                    zh: '走开啊',
+                    en: '',
+                    jp: ''
+                }
+            }
+        ]
+    },
+    {
+        clfy: {zh: '小希叫你起床', en: '', jp: ''},
+        alias: {zh: '\"叫床\"(bushi', en: '', jp: ''},
+        voice:[
+            {
+                path: 'jiaoqichuang-xx.mp3',
+                desc: {
+                    zh: '叫起床',
+                    en: 'wakeup',
+                    jp: ''
+                }
+            },
+            {
+                path: 'bunengzaishuila-xx.mp3',
+                desc: {
+                    zh: '不能再睡啦！快起床吧',
+                    en: ''
+                }
+            },
+            {
+                path: 'xhwnx-2v5de.mp3',
+                desc: {
+                    zh: '早上好！早上啦！今天一天也要元气满满哦！',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'aadve-opxnv.mp3',
+                desc: {
+                    zh: '再睡下去的话，小希会生气哦',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: '04cok-az8or.mp3',
+                desc: {
+                    zh: '喂喂喂，再不起来要迟到了',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'ciahh-yd7v0.mp3',
+                desc: {
+                    zh: '喵喵喵喵喵喵喵喵喵',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'ev4lh-wl804.mp3',
+                desc: {
+                    zh: '今天你也很努力啦！好好休息吧！',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: '9a56p-a6toc.mp3',
+                desc: {
+                    zh: '晚安，明天的小希也会叫你起床哦',
+                    en: '',
+                    jp: ''
+                }
+            }
+        ]
+    },
+    {
+        clfy: {zh: '小希的鼓励', en: '', jp: ''},
+        alias: {zh: '希爹你看看你过去多励志', en: '', jp: ''},
+        voice:[
+            {
+                path: '7jg07-m2fp9.mp3',
+                desc: {
+                    zh: 'hi，我是虚拟up主小希',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'ehhd2-9v0ko.mp3',
+                desc: {
+                    zh: '奔跑吧，怀抱着重要的东西',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'dkzad-nnkm8.mp3',
+                desc: {
+                    zh: '不论风雨，小希一直都在你的身边',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'ujma2-p9ezg.mp3',
+                desc: {
+                    zh: '世界很大，也很美丽！你不这么觉得吗？',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'owr4h-fyix0.mp3',
+                desc: {
+                    zh: '谁都有失败，但是一定可以再重启',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: 'tcsp1-dt6p6.mp3',
+                desc: {
+                    zh: '因为每天都只有二十四小时一千四百四十分钟',
+                    en: '',
+                    jp: ''
+                }
+            },
+            {
+                path: '5ftr9-ww1pn.mp3',
+                desc: {
+                    zh: '只靠一个人无法战斗，小希也在陪着你！',
+                    en: '',
+                    jp: ''
+                }
+            }
+        ]
+    }
+]
