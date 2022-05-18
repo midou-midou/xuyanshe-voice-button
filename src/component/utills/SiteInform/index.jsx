47e61963cb895f9d95e359b5e514b1e2edad7421
MIMONATCH <15500615701@163.com>
2022-05-18 21:45:33 +0800

    fix: 主页inform没有时还显示通知横幅

--- src/component/utills/SiteInform/index.jsx
+++ src/component/utills/SiteInform/index.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { FormattedMessage, IntlProvider } from "react-intl";
 import { useSelector } from "react-redux";
 
@@ -5,15 +6,22 @@ function SiteInform() {
     const lang = useSelector((state) => state.getLang)
     const siteInfo = useSelector((state) => state.getSiteInfo.inform)
 
-    return ( 
-        <div className="siteInform-container">
-            <div className="siteInform-inner">
-                <IntlProvider locale={lang} messages={siteInfo}>
-                    <FormattedMessage id={lang}></FormattedMessage>
-                </IntlProvider>
+    if(siteInfo.zh === "" && siteInfo.en === "" && siteInfo.jp === ""){
+        return (
+            <Fragment></Fragment>
+        )
+    }else{
+        return (
+            <div className="siteInform-container">
+                <div className="siteInform-inner">
+                    <IntlProvider locale={lang} messages={siteInfo}>
+                        <FormattedMessage id={lang}></FormattedMessage>
+                    </IntlProvider>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+   
 }
 
 export default SiteInform;
