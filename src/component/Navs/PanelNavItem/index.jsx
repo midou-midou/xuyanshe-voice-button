fb26ee841ef9ed0c19c64d3088dd4ff2b7e0fac1
midou <midoude163@163.com>
2023-05-08 20:05:30 +0800

    fix: 小屏导航图片显示问题

--- src/component/Navs/PanelNavItem/index.jsx
+++ src/component/Navs/PanelNavItem/index.jsx
@@ -6,7 +6,7 @@ function PanelNavItem(props) {
             <NavLink 
                 activeClassName="sidebar-item-active" 
                 className="sidebar-vup-icon" 
-                style={{backgroundImage: `url(https://xysbtn.xiaoblogs.cn/profile/${props.vup.uid}.webp)`}} 
+                style={{backgroundImage: `url(https://voice.xuyanshe.club/profile/${props.vup.uid}.webp)`}} 
                 to={`/${props.vup.alias}`}
             ></NavLink>
         </div>
